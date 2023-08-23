42c9967dcf4f3cbbbafdc483f352e7e5a63d885e
Naufal Fawwaz <fawaznaufal23@gmail.com>
2023-08-23 17:07:50 +0700

    Initial project commit

---
+++ Aquacare/AquacareApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquacareApp.swift
+//  Aquacare
+//
+//  Created by Naufal Fawwaz Andriawan on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AquacareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
